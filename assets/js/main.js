3d56aae432287467aad7b709fa807289620aa754
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2023-12-22 18:04:44 +0530

    feat: Display all localStorage datas in UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ import JustValidate from "just-validate";
 
 const formEl = document.getElementById("courier-request-form");
 
+const localStorageKey = "courierData";
+
 const validateForm = new JustValidate(formEl, {
   validateBeforeSubmitting: true,
 });
@@ -91,13 +93,56 @@ validateForm.onSuccess(() => {
     existingCourierArray.push(formValueObj);
 
     // push the new array (which has all the info to the localstorage)
-    localStorage.setItem("courierData", JSON.stringify(existingCourierArray));
+    localStorage.setItem(localStorageKey, JSON.stringify(existingCourierArray));
   } else {
     newCourierData.push(formValueObj);
 
-    localStorage.setItem("courierData", JSON.stringify(newCourierData));
+    localStorage.setItem(localStorageKey, JSON.stringify(newCourierData));
   }
 
   alert("Courier Request submitted successfully!");
   formEl.reset();
 });
+
+function getAllCourierDatas() {
+  // Get all stored courier datas which are available in localStorage
+  const courierData = localStorage.getItem(localStorageKey);
+
+  const courierDataArr = JSON.parse(courierData);
+
+  if (courierDataArr) {
+    //   write those values into the table ui.
+    const tableEl = document.querySelector("#courierDataTable");
+
+    const finalData = courierDataArr
+      .map((courierData) => {
+        return `
+    <tr>
+      <td class="px-2 py-1 border">${courierData.name}</td>
+      <td class="px-2 py-1 border">${courierData.mobile}</td>
+      <td class="px-2 py-1 border">${courierData["pickup-date"]}</td>
+      <td class="px-2 py-1 border">${courierData["pickup-area"]}</td>
+      <td class="px-2 py-1 border">
+          <button
+          type="button"
+          class="px-2 py-1 rounded bg-red-500 hover:bg-red-600 text-white text-sm">
+          Delete
+      </button>
+      </td>
+    </tr>
+    `;
+      })
+      .join(" ");
+
+    console.log(finalData);
+
+    tableEl.innerHTML += finalData;
+    //   console.log(courierDataArr);
+
+    // display the UI with those datas.
+  } else {
+    console.log("No value available on localStorage");
+  }
+}
+
+getAllCourierDatas();
